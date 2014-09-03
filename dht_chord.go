f071e501976268e7239145021ad3598bc3319203
Simon <simon.ekstrom@gmail.com>
2014-09-03 18:04:54 +0200

    Initial.

---
+++ dht_chord.go
@@ -0,0 +1,39 @@
+// chord
+package dht
+
+import ()
+
+var newid string
+
+//var newid string
+
+type Node struct {
+	nodeId   string
+	nodeIp   string
+	nodePort string
+}
+
+func makeDHTNode(id string, ip string, port string) Node {
+	if id == nil {
+		newid = generateNodeId()
+		return Node{newid, ip, port}
+	} else {
+		return Node{id, ip, port}
+	}
+}
+
+func (n *Node) addToRing(node Node) {
+
+}
+
+func (n *Node) printRing() {
+
+}
+
+func (n *Node) testCalcFingers(i int, j int) {
+
+}
+
+func (n *Node) lookup(hashKey string) Node {
+	return Node{"test1", "test2", "test3"} // returnera rätt node här
+}
