d9dbadc103e8207fe0c712469a3d3841a08d13a4
Johan Forsling <ojaofi-0@student.ltu.se>
2014-09-03 20:37:29 +0200

    Changed the return type of makeDHTNode to a node-pointer, and made some minor changes to function printRing.

--- dht_chord.go
+++ dht_chord.go
@@ -12,29 +12,25 @@ type Node struct {
 	successor *Node
 }
 
-func makeDHTNode(id *string, ip string, port string) Node {
+func makeDHTNode(id *string, ip string, port string) *Node {
 	if id == nil {
 		newid := generateNodeId()
-		return Node{newid, ip, port, nil}
+		return &Node{newid, ip, port, nil}
 	} else {
-		return Node{*id, ip, port, nil}
+		return &Node{*id, ip, port, nil}
 	}
 }
 
-func (n *Node) addToRing(node Node) {
+func (n *Node) addToRing(node *Node) {
 	responsibleNode := n.lookup(node.nodeId)
 	node.successor = responsibleNode.successor
-	responsibleNode.successor = &node
+	responsibleNode.successor = node
 }
 
 func (n *Node) printRing() {
-	for i := n.successor; i != nil; i = i.successor {
-		if i == n {
-			fmt.Println(i.nodeId + "\n")
-			return
-		} else {
-			fmt.Println(i.nodeId + "\n")
-		}
+	fmt.Println(n.nodeId + "\n")
+	for i := n.successor; i != nil && i != n; i = i.successor {
+		fmt.Println(i.nodeId + "\n")
 	}
 }
 
