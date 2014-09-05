b4d0853f35231fdc1e0c8334b8a8c5ff18844c0e
Johan Forsling <ojaofi-0@student.ltu.se>
2014-09-05 08:10:26 +0200

    Added functionality to the lookup function.

--- dht_chord.go
+++ dht_chord.go
@@ -9,15 +9,20 @@ type Node struct {
 	nodeId    string
 	nodeIp    string
 	nodePort  string
+	finger    []*Node
 	successor *Node
 }
 
 func makeDHTNode(id *string, ip string, port string) *Node {
 	if id == nil {
 		newid := generateNodeId()
-		return &Node{newid, ip, port, nil}
+		newNode := Node{newid, ip, port, nil, nil}
+		newNode.successor = &newNode // add yourself as successor
+		return &newNode
 	} else {
-		return &Node{*id, ip, port, nil}
+		newNode := Node{*id, ip, port, nil, nil}
+		newNode.successor = &newNode
+		return &newNode
 	}
 }
 
@@ -34,10 +39,18 @@ func (n *Node) printRing() {
 	}
 }
 
-func (n *Node) testCalcFingers(i int, j int) { // i = ?? j = number of bits
-
+func (n *Node) testCalcFingers(k int, m int) { // k = finger index, m = number of bits
+	// calcFinger(n []byte, k int, m int)
 }
 
-func (n *Node) lookup(hashKey string) Node {
-	return Node{"test1", "test2", "test3", nil} // returnera rätt node här
+func (n *Node) lookup(key string) *Node {
+	id1 := []byte(n.nodeId)
+	id2 := []byte(n.successor.nodeId)
+	keyId := []byte(key)
+
+	if between(id1, id2, keyId) {
+		return n
+	} else {
+		return n.successor.lookup(key)
+	}
 }
