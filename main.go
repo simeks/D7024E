c6bf9f9da6d091a58b8188009d25de10d28430d1
Simon <simon.ekstrom@gmail.com>
2014-10-05 19:41:32 +0200

    Added two command-line arguments, "-p <port>", specifies local port. "-j <remote host>", specifies a remote host to join.

--- main.go
+++ main.go
@@ -8,35 +8,32 @@ import (
 func main() {
 	app := App{}
 
-	if len(os.Args) > 1 {
-		if os.Args[1] == "server" {
-			app.init("127.0.0.1", "13337")
-
-			go func() {
-				http.HandleFunc("/chord/", chordHandler)
-				http.HandleFunc("/inserted/", func(w http.ResponseWriter, r *http.Request) {
-					insertedHandler(w, r, &app)
-				})
-				http.ListenAndServe(":13337", nil)
-			}()
-
-			app.listen()
-
-		} else if os.Args[1] == "client" {
-			app.init("127.0.0.1", os.Args[2])
-
-			go app.join("127.0.0.1:13337")
-
-			go func() {
-				http.HandleFunc("/chord/", chordHandler)
-				http.HandleFunc("/inserted/", func(w http.ResponseWriter, r *http.Request) {
-					insertedHandler(w, r, &app)
-				})
-				http.ListenAndServe(":"+os.Args[2], nil)
-			}()
-
-			app.listen()
-
+	port := "13337"
+	remote := ""
+	for i := 1; i < len(os.Args); i++ {
+		if os.Args[i] == "-p" && (i+1) < len(os.Args) { // Port <port number>
+			port = os.Args[i+1]
+			i++
+		} else if os.Args[i] == "-j" && (i+1) < len(os.Args) { // Join <remote host>
+			remote = os.Args[i+1]
+			i++
 		}
 	}
+
+	app.init("127.0.0.1", port)
+
+	if remote != "" { // Join existing ring
+		go app.join(remote)
+	}
+
+	go func() {
+		http.HandleFunc("/chord/", chordHandler)
+		http.HandleFunc("/inserted/", func(w http.ResponseWriter, r *http.Request) {
+			insertedHandler(w, r, &app)
+		})
+		http.ListenAndServe(":"+port, nil)
+	}()
+
+	app.listen()
+
 }
