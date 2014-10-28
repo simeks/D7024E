98e1355b5eea7eb75b0f82be8516f191ebea8a28
Johan Forsling <ojaofi-0@student.ltu.se>
2014-10-28 16:05:10 +0100

    You now also have to give an encryption key when storing a key-value pair.

--- http.go
+++ http.go
@@ -2,38 +2,44 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"encoding/json"
 )
 
 func chordHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Insert a new key/value pair</h1>"+
 		"<form action=\"/post/\" method=\"POST\">"+
-		"value:"+
+		"Value:"+
 		"<textarea name=\"insertvalue\"></textarea><br>"+
-		"key:"+
+		"Key:"+
 		"<textarea name=\"insertkey\"></textarea><br>"+
+		"Encryption key:"+
+		"<textarea name=\"insertencryptionkey\"></textarea><br>"+
 		"<input type=\"submit\" value=\"Submit\">"+
 		"</form>"+
 		"<h1>Delete a key/value pair</h1>"+
 		"<form action=\"/delete/\" method=\"POST\">"+
-		"key:"+
+		"Key:"+
 		"<textarea name=\"deletekey\"></textarea><br>"+
 		"<input type=\"submit\" value=\"Submit\">"+
 		"</form>"+
 		"<h1>Update value for key</h1>"+
 		"<form action=\"/put/\" method=\"POST\">"+
-		"value:"+
+		"Value:"+
 		"<textarea name=\"updatevalue\"></textarea><br>"+
-		"key:"+
+		"Key:"+
 		"<textarea name=\"updatekey\"></textarea><br>"+
+		"Encryption key:"+
+		"<textarea name=\"updateencryptionkey\"></textarea><br>"+
 		"<input type=\"submit\" value=\"Submit\">"+
 		"</form>"+
 		"<h1>Get the value for key</h1>"+
 		"<form action=\"/get/\" method=\"POST\">"+
-		"key:"+
+		"Key:"+
 		"<textarea name=\"getkey\"></textarea><br>"+
+		"Decryption key:"+
+		"<textarea name=\"getdecryptionkey\"></textarea><br>"+
 		"<input type=\"submit\" value=\"Submit\">"+
 		"</form>")
 }
@@ -41,7 +47,11 @@ func chordHandler(w http.ResponseWriter, r *http.Request) {
 func postHandler(w http.ResponseWriter, r *http.Request, app *App) {
 	value := r.FormValue("insertvalue")
 	key := r.FormValue("insertkey")
+	//encryptionkey := r.FormValue("insertencryptionkey")
 	hashkey := sha1hash(key)
+
+	// kör AES så value blir krypterat
+	// ...
 
 	responsibleNode := app.lookup(hashkey)
 
@@ -97,7 +107,7 @@ func deleteHandler(w http.ResponseWriter, r *http.Request, app *App) {
 		if r != nil {
 			reply := DeleteValueReply{}
 			json.Unmarshal(r.Data, &reply)
-			
+
 			if reply.Deleted {
 				fmt.Fprintf(w, "<p><a href=\"/chord/\">go back</a></p>"+
 					"<p>Key deleted successfully!</p>")
@@ -111,6 +121,7 @@ func deleteHandler(w http.ResponseWriter, r *http.Request, app *App) {
 
 func getHandler(w http.ResponseWriter, r *http.Request, app *App) {
 	key := r.FormValue("getkey")
+	//decryptionkey := r.FormValue("getdecryptionkey")
 	hashkey := sha1hash(key)
 
 	responsibleNode := app.lookup(hashkey)
@@ -118,6 +129,8 @@ func getHandler(w http.ResponseWriter, r *http.Request, app *App) {
 	if bytes.Compare(app.node.nodeId, responsibleNode.nodeId) == 0 {
 		_, ok := app.keyValue[hashkey]
 		if ok {
+			// decrypta app.keyValue[hashkey]
+			// ...
 			fmt.Fprintf(w, "<p><a href=\"/chord/\">go back</a></p>"+
 				"<p>Value: "+app.keyValue[hashkey]+"</p>")
 		} else {
@@ -131,7 +144,6 @@ func getHandler(w http.ResponseWriter, r *http.Request, app *App) {
 		bytes, _ := json.Marshal(req)
 		r := app.transport.sendRequest(responsibleNode.addr, "getKey", bytes)
 
-
 		if r == nil {
 			fmt.Println("Call error (getKey)")
 			return
@@ -142,20 +154,23 @@ func getHandler(w http.ResponseWriter, r *http.Request, app *App) {
 			json.Unmarshal(r.Data, &reply)
 
 			if reply.Value != "" {
+				// decrypta reply.Value
+				// ...
 				fmt.Fprintf(w, "<p><a href=\"/chord/\">go back</a></p>"+
 					"<p>Value: "+reply.Value+"</p>")
 				return
 			}
-		} 
+		}
 		fmt.Fprintf(w, "<p><a href=\"/chord/\">go back</a></p>"+
 			"<p>Key was not found.</p>")
-	
+
 	}
 }
 
 func putHandler(w http.ResponseWriter, r *http.Request, app *App) {
 	value := r.FormValue("updatevalue")
 	key := r.FormValue("updatekey")
+	//encryptionkey := r.FormValue("updateencryptionkey")
 	hashkey := sha1hash(key)
 
 	responsibleNode := app.lookup(hashkey)
